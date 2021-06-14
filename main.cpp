5759b453044d61ba543d41eef4e88c5af48e7d9f
Glenn <55197203+castilloglenn@users.noreply.github.com>
2021-06-14 20:20:35 +0800

    added documentation

--- main.cpp
+++ main.cpp
@@ -5,37 +5,54 @@
 using namespace std;
 
 
+/**
+    Tictactoe Game (Optimized Algorithm)
+    Group #2: Team Purple
+    Members:
+    • Bernal, Nivea Rea
+    • Cabigat, Ernest John
+    • Castillo, Allen Glenn E.
+    • Garcia, Sebastian Raphael
+ */
+
+
+
 string winner;
 string board[9];
 string validPieces[2] = {"X", "O"};
 int boardSize = sizeof(board)/sizeof(board[0]);
-int emptyFields = boardSize, turn = 0;
+int emptyFields = boardSize, turn = 0, game = 0;
 int scores[3] = {0, 0, 0};
 bool gameFinished = false, playerAI = false;
 
 // evaluator array
 string *eval[8][3] = {
+    // horizontal
     {&board[0], &board[1], &board[2]},
     {&board[3], &board[4], &board[5]},
     {&board[6], &board[7], &board[8]},
+
+    // vertical
     {&board[0], &board[3], &board[6]},
     {&board[1], &board[4], &board[7]},
     {&board[2], &board[5], &board[8]},
+
+    // diagonal
     {&board[0], &board[4], &board[8]},
     {&board[2], &board[4], &board[6]}
 };
 
 // constant access array
 int indexFields[9][4] = {
-    {0, 3,  6, -1},
-    {0, 4, -1, -1},
-    {0, 5,  7, -1},
-    {1, 3, -1, -1},
-    {1, 4,  6,  7},
-    {1, 5, -1, -1},
-    {2, 3,  7, -1},
-    {2, 4, -1, -1},
-    {2, 5,  6, -1}
+    {0, 3,  6, -1}, // 1 or index 0
+    {0, 4, -1, -1}, // 2
+    {0, 5,  7, -1}, // 3
+    {1, 3, -1, -1}, // 4
+    {1, 4,  6,  7}, // 5
+    {1, 5, -1, -1}, // 6
+    {2, 3,  7, -1}, // 7
+    {2, 4, -1, -1}, // 8
+    {2, 5,  6, -1}  // 9
 };
 
 // method prototyping
@@ -62,8 +79,10 @@ int main() {
     bool gameRunning = true;
     while (gameRunning) {
         clearBoard();
+        game++;
         // This is for player-2-player
         while (!gameFinished) {
+
             printBoard();
 
             int move;
@@ -109,14 +128,14 @@ void clearBoard() {
 void printBoard() {
     system("cls");
     printf(
-        "\n\n\n\t\t Tic-Tac-Toe\t    GUIDE\n"
+        "\n\n\n\t\t Tic-Tac-Toe\t    GUIDE\t      Game %02d\n"
         "\t\t+---+---+---+\t+---+---+---+\n"
         "\t\t| %s | %s | %s |\t| 1 | 2 | 3 |\tPlayer 1: %3d points\n"
         "\t\t+---+---+---+\t+---+---+---+\n"
         "\t\t| %s | %s | %s |\t| 4 | 5 | 6 |\tPlayer 2: %3d points\n"
         "\t\t+---+---+---+\t+---+---+---+\n"
         "\t\t| %s | %s | %s |\t| 7 | 8 | 9 |\tDraws:    %3d rounds\n"
-        "\t\t+---+---+---+\t+---+---+---+\n\n",
+        "\t\t+---+---+---+\t+---+---+---+\n\n", game,
         board[0].c_str(), board[1].c_str(), board[2].c_str(), scores[0],
         board[3].c_str(), board[4].c_str(), board[5].c_str(), scores[1],
         board[6].c_str(), board[7].c_str(), board[8].c_str(), scores[2]
